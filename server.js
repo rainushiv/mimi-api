bf37b70804477808398f5f3889f9140df911bc31
rainushiv <shivpreet567@gmail.com>
2024-10-14 00:41:15 -0400

    init

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use(express.static('images'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'shivalry.dev');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
